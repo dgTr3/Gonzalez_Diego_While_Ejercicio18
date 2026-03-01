1003d163372b0bce02cb701e2cd007c7f55302d2
dgTr3 <Edugta31@gmail.com>
2026-03-01 14:53:59 -0600

    completo

--- While_Ejercicio18/Program.cs
+++ While_Ejercicio18/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("");
+﻿Console.WriteLine("Calcular el factorial de un número ingresado.");
+Console.WriteLine("Ingrese número");
+int numero = int.Parse(Console.ReadLine());
+int i = 1;
+int factorial = 1;
+while (i <= numero)
+{
+    factorial *= i;
+    i++;
+}
+Console.WriteLine("Factorial"+ factorial);
